1b077fe5932664fdb3a9cc602dea5c30142ca2f0
a_sontica <alexandru.sontica@mindgeek.com>
2021-04-15 00:12:18 +0300

    user model

--- time-management-backend/src/main/java/com/alexandru/timemanagement/model/User.java
+++ time-management-backend/src/main/java/com/alexandru/timemanagement/model/User.java
@@ -1,7 +1,46 @@
 package com.alexandru.timemanagement.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Integer id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private RoleEnum role;
+
+    @Getter
+    public enum RoleEnum {
+        USER ("USER"),
+        MANAGER ("MANAGER"),
+        ADMIN ("ADMIN"),
+        ;
+
+        private final String role;
+
+        RoleEnum(String role) {
+            this.role = role;
+        }
+    }
 }
